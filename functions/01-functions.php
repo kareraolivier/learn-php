254a2e1fa78b6b1a3f954f89fa806bd87f2214ea
kareraolivier <kareraolivier@gmail.com>
2024-02-26 19:20:02 +0200

    funtion: 01-normal and arrow function

---
+++ functions/01-functions.php
@@ -0,0 +1,32 @@
+<?php
+echo "functions \n";
+
+function greatings()
+{
+    echo 'hello';
+};
+greatings();
+
+
+
+// accessing global variable
+$x = 12;
+function add($nbr1 = 2, $nbr3 = 4)
+{
+    global $x;
+    echo $x;
+    return $nbr1 + $nbr3;
+};
+echo add(98, 67);
+
+
+//Arrow function 
+$message = fn () => {
+    if (2 == 2) {
+        return 'olivier';
+    }
+    return 'karera';
+};
+
+echo $message();
+
